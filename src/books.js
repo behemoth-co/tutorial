c2de9efe363ef4fd4ee34745b16a09346e000199
behemoth-co <behemoth.collab@gmail.com>
2023-04-10 13:29:08 +0300

    first-commit

---
+++ src/books.js
@@ -0,0 +1,24 @@
+import img1 from './images/book-1.jpg'
+import img2 from './images/book-2.jpg'
+import img3 from './images/book-3.jpg'
+
+export const books = [
+    {
+        author: "Dav Pilkey",
+        title: "Dog Man: Twenty Thousand Fleas Under the Sea",
+        img: img1,
+        id: 1,
+    },
+    {
+        author: "Peter Attia MD, Bill Gifford",
+        title: "Outlive: The Science and Art of Longevity",
+        img: img2,
+        id: 2,
+    },
+    {
+        author: "James Clear",
+        title: "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones",
+        img: img3,
+        id: 3,
+    },
+    ];
